04fce92888149e18570d068b78cfe59726ce00a3
Daniel Yuan <danielyuan@gmail.com>
2017-12-06 04:26:48 +0000

    Made all relative paths absolute

--- src/constants.py
+++ src/constants.py
@@ -2,15 +2,20 @@
 # @author: Daniel Yuan
 # @brief: Holds global constants
 
+import os
+import __main__
+
+WORKING_DIR = os.path.abspath(os.path.dirname(__main__.__file__))
+
 # .env file that holds private variables
-ENV_FILE = '.env'
+ENV_FILE = os.path.join(WORKING_DIR, '.env')
 
 # .server file that holds all server information
-SERVER_FILE = '.server'
+SERVER_FILE = os.path.join(WORKING_DIR, '.server')
 
 # Default Screen Name
 DEFAULT_SCREEN_NAME = 'DanBotServer'
 
 # Default Logs
-DEFAULT_LOG_FOLDER = './logs'
+DEFAULT_LOG_FOLDER = os.path.join(WORKING_DIR, 'logs')
 DEFAULT_LOG_FILENAME = 'DanBot'
